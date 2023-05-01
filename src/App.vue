2d7ddff577f921d3cae3b397fc2c90c50606f3d7
theam-dev <phearakroun@gmail.com>
2023-05-01 13:45:26 +0700

    Custom Css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-   <input type="file" @change="onFileChange" />
-  <div id="preview">
-    <img v-if="url" :src="url" width="80"/>
+  <div class="viewimg">
+    <img width="150" height="150" :src="url" class="preview" />
+    <label class="label">
+      <input type="file" @change="onFileChange" />
+      <span>Select a file</span>
+    </label>
   </div>
 </template>
-
 <script>
-
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
-      url: null,
+      url: 'http://placehold.it/150x150',
     }
   },
   methods: {
-    onFileChange (e) {
-      const file = e.target.files [0];
-      this.url = URL.createObjectURL (file);
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.url = URL.createObjectURL(file);
     }
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -31,5 +31,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.viewimg {
+  display: inline-grid;
+}
+label.label input[type="file"] {
+  position: absolute;
+  top: -1000px;
+}
+.label,
+.preview {
+  cursor: pointer;
+  border: 1px solid #cccccc;
+  border-radius: 5px;
+  margin: 5px;
+  background: #dddddd;
+  display: inline-block;
+}
+.label{
+  padding: 5px 15px;
 }
 </style>
